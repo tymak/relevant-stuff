675b4053c58d42dbdd3b8682ce0d6c9ef1b16ae0
unknown <timko.michal@centrum.cz>
2019-09-26 18:00:07 +0200

    oprava mapy

--- Hously/map/mapa.js
+++ Hously/map/mapa.js
@@ -1,39 +1,83 @@
+const vstupniData = {
+    listofAdres: [],
+    listofId: []
+};
+fetch("./map/api")
+    .then(response => response.json())
 
+    //prevod dat na spravny tvar
+    .then(data => {
+        data.map(element => {
+            const adresa =
+                `${element.street}` +
+                ` ` +
+                `${element.house_number}` +
+                `,` +
+                `${element.postal}` +
+                `,` +
+                `${element.city}`;
 
+            vstupniData.listofAdres.push(adresa);
+            vstupniData.listofId.push(element.id);
+        });
 
-//vstupni data
-const listofAdress=["U hajovny 11, 18200, Praha","Václavské náměstí 1, Praha",]
+        //**********************************************- */
+        //vytvoreni nove mapy
+        const center = SMap.Coords.fromWGS84(14.4304, 50.07975);
+        const map = new SMap(JAK.gel("map"), center, 11, {
+            height: "50vh",
+            width: "100vw"
+        });
 
-const center =SMap.Coords.fromWGS84(14.4676344,50.1281042);
-const map = new SMap(JAK.gel("map"), center, 10);
-map.addDefaultLayer(SMap.DEF_BASE).enable();
-map.addDefaultControls();
+        map.addDefaultLayer(SMap.DEF_BASE).enable();
+        map.addDefaultControls();
 
-const layer = new SMap.Layer.Marker();
-map.addLayer(layer);
-layer.enable();
+        const layer = new SMap.Layer.Marker();
+        map.addLayer(layer);
+        layer.enable();
+        //*************************************** */
+        // vytvoreni sady markerů z listu adres
+        for (let i = 0; i < vstupniData.listofAdres.length; i++) {
+            console.log("hello");
+            new SMap.Geocoder(vstupniData.listofAdres[i], odpoved, {
+                card_id: vstupniData.listofId[i]
+            });
+        }
+        // vytvoreni jednotlivych markerů
 
+        function odpoved(geocoder) {
+            const vysledky = geocoder.getResults()[0].results;
 
+            const pozice = vysledky[0].coords;
 
-// vytvoreni jednotlivych markerů
-function odpoved(geocoder) {
-    const vysledky= geocoder.getResults()[0].results;
-    console.log("call", vysledky);
-    
-    const pozice=vysledky[0].coords;
-    const marker = new SMap.Marker(pozice, `marker-${vysledky[0].id}`);
-    console.log(marker);
-    layer.addMarker(marker);
+            const znacka = JAK.mel("div");
+            const obrazek = JAK.mel(
+                "img",
+                {
+                    src: "../img/hously-logo-small.png"
+                },
+                {
+                    width: "35px",
+                    height: "35px"
+                }
+            );
+            znacka.appendChild(obrazek);
+            const card = new SMap.Card();
+            console.log("geocoder", geocoder._options.card_id);
 
-}
-// vytvoreni sady markerů z listu adres
-listofAdress.map(element=>{
-    new SMap.Geocoder(element, odpoved);
-})
+            card.getHeader().innerHTML = `<div class=visit_card>
+                                        <img src="../img/hously-logo-small.png">
+                                        <div class=card_title> Hously s.r.o</div>
+                                        </div>`;
 
-  
+            card.getBody().innerHTML = geocoder._query;
+            card.getFooter().innerHTML = `<a href="/houses#${
+                geocoder._options.card_id
+            }" src="/building_id${geocoder._options.card_id}">home</a>`;
+            const marker = new SMap.Marker(pozice, null, { url: znacka });
+            marker.decorate(SMap.Marker.Feature.Card, card);
 
-
-
-
-
+            layer.addMarker(marker);
+        }
+        //******************************************** */
+    });
